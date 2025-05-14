3e0a0153a99ebf108ec072fb527a2046cf583a98
msaimnaveed5384 <msaimnaveed5384@gmail.com>
2025-05-15 02:39:40 +0500

    Update Army.cpp

--- Army.cpp
+++ Army.cpp
@@ -19,10 +19,21 @@ void Army::feed() {
 	morale += 1;
 	cout << "Army fed properly. Morale increased." << endl;
 }
-void Army::pay() {
-	morale += 3;
-	cout << "Army paid. Morale increased significantly." << endl;
+void Army::pay(Economy& eco) 
+{
+	int payment = soldiers * 5;
+	if (eco.treasury >= payment) 
+	{
+		eco.treasury -= payment;
+		morale += 3;
+		cout << "Army paid. Morale increased significantly." << endl;
+	}
+	else {
+		morale -= 5;
+		cout << "Treasury too low to pay army! Morale dropped!" << endl;
+	}
 }
+
 
 void Army::status_report() const {
 	cout << "Army: " << soldiers << " soldiers, Morale: " << morale << endl;
